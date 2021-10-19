a2e2a8e02c4b6a81413628256abe8f0ead384f20
Wiktor Maciążek <75098062+swapperDEV@users.noreply.github.com>
2021-10-19 16:56:22 +0200

    v1.4 fix

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,8 +3,7 @@ filter.addEventListener('keyup', () => {
     let array = document.querySelectorAll('li')
     array = Array.prototype.slice.call(array);
     array.forEach((e) => {
-        if(e.textContent.includes(filter.value)) {
-            console.log(e.textContent + ' OK');
+        if(e.textContent.toLowerCase().includes(filter.value.toLowerCase())) {
             e.style.display = 'flex'
         } else {
             e.style.display = 'none'
